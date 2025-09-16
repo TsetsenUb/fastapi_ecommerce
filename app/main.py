d3ea1061b90ad50c6e0130347a24f813b56c4cfb
Ubushiev Tsetsen <tsetsn@yandex.ru>
2025-09-16 15:17:41 +0300

    adding logging

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from uuid import uuid4
+from fastapi import FastAPI, Request, BackgroundTasks
+from fastapi.responses import JSONResponse
+from loguru import logger
 
 from app.routers import categories, products, users, reviews
 
@@ -9,6 +12,15 @@ app = FastAPI(
     version="0.1.0",
 )
 
+
+logger.add(
+    "info.log",
+    format="Log: [{extra[log_id]}:{time} - {level} - {message}]",
+    level="INFO",
+    enqueue=True
+)
+
+
 # Подключаем маршруты категорий и товаров
 app.include_router(categories.router)
 app.include_router(products.router)
@@ -16,9 +28,25 @@ app.include_router(users.router)
 app.include_router(reviews.router)
 
 
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
+
+
 # Корневой эндпоинт для проверки
 @app.get("/")
-async def root():
+async def root(background_tasks: BackgroundTasks):
     """
     Корневой маршрут, подтверждающий, что API работает.
     """
